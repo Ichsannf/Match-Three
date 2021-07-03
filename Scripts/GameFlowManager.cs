ad779d38ceb1a0670d3b0ada7caa3e9afaca6aeb
Ichsannf <ichsanfauzi354@gmail.com>
2021-07-03 07:51:08 +0700

    first commit

---
+++ Scripts/GameFlowManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFlowManager : MonoBehaviour
+{
+   
+    #region Singleton
+
+    private static GameFlowManager _instance = null;
+
+    public static GameFlowManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<GameFlowManager>();
+
+                if (_instance == null)
+                {
+                    Debug.LogError("Fatal Error: GameFlowManager not Found");
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    #endregion
+
+    [Header("System")]
+    public BoardManager board;
+    public TimeManager time;
+    public ScoreManager score;
+
+    [Header("UI")]
+    public UiGameOver GameOverUI;
+
+    public bool IsGameOver { get { return isGameOver; } }
+
+    private bool isGameOver = false;
+
+    public void GameOver()
+    {
+        isGameOver = true;
+        ScoreManager.Instance.SetHighScore();
+        GameOverUI.Show();
+    }
+
+}
